c4c61f82fe71f52e9646ab220f8a24ff79098644
Mathieu Clément <mcfly1893@gmail.com>
2019-11-15 15:30:58 +0100

    Added comments to JS script

--- assets/js/butttons.js
+++ assets/js/butttons.js
@@ -1,7 +1,11 @@
+// Get secondary buttons
 const secondaryButtons = document.getElementsByClassName("btn--secondary");
+// Get input file elements and triggers
 const fileInputs = document.getElementsByClassName("input__file");
 const fileButtons = document.getElementsByClassName("input__file-trigger");
 
+// Change the displayed text in the button by the swap text and save original (type ==  "on")
+// Change it back to the original text (type == "out")
 function changeElementText(element, type) {
   console.log(element);
   if (type == "on") {
@@ -12,6 +16,9 @@ function changeElementText(element, type) {
   }
 }
 
+// Add event listeners to secondary buttons on the page
+// When focus or mouseover -> use changeElementText() with type == "on"
+// When focusout or mouseout -> use changeElementText() with type =="out"
 for (let index = 0; index < secondaryButtons.length; index++) {
   const button = secondaryButtons[index];
   button.addEventListener("mouseover", function() {
@@ -28,9 +35,11 @@ for (let index = 0; index < secondaryButtons.length; index++) {
   }, false);
 }
 
+// Add event listener to all input file elements on the page
+// When change, replace the text of the trigger by the selected file name
 for (let index = 0; index < fileInputs.length; index++) {
   const input = fileInputs[index];
-  input.addEventListener( "change", function() {  
+  input.addEventListener("change", function() {  
     fileButtons[index].innerHTML = this.value.split("\\").pop();  
   });  
 }
